bf9462d3fc3ab59f39a652f58045fa697af06e3e
Mauricio Duran Torres <mauriciodurant@gmail.com>
2018-10-10 23:34:24 -0300

    register controller and login controller with Postman api testing

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -7,13 +7,27 @@
 /**
  * Resourceful controller for interacting with auths
  */
+
+const User = use('App/Models/User');
 class AuthController {
 
-    async login() {
+    async login({ request, response, auth }) {
+        const { email, password } = request.all();
+        const user = await auth.attempt(email, password);
+        return response.json(user);
 
     }
 
-    async register() {
+    async register({ request, response, auth }) {
+        const user = new User;
+        user.username = request.input('username');
+        user.email = request.input('email');
+        user.password = request.input('password');
+
+        await user.save(); // Guardar
+        //await auth.generate(user); //Generar usuario y generar Token
+
+        return response.json(user);
 
     }
 }
