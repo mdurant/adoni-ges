e15ad155abe9f0c03526995be256e65e71669b03
Mauricio Duran Torres <mauriciodurant@gmail.com>
2018-10-10 21:36:47 -0300

    Se crea Controller Auth y modificaciones en las rutas para definir el grupo de versión de api a trabajar, la condifuración en app y el controlador Auth con metodos de login y register

---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with auths
+ */
+class AuthController {
+
+    async login() {
+
+    }
+
+    async register() {
+
+    }
+}
+
+module.exports = AuthController
